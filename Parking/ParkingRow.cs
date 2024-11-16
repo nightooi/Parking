a548346dfa3feaae7779aa03e6047892d33d1275
nightooi <rafaellost2021@outlook.com>
2024-11-16 16:20:01 +0100

    Config class definition done

--- Parking/ParkingRow.cs
+++ Parking/ParkingRow.cs
@@ -132,10 +132,13 @@ public class ParkingRow : IParkingRow<IParkingSpace>
         {
              //Assumes single line of PropertyDenoterPropertyNameValueStartValue
              public static readonly string PropertyDenoter = "%%%0x0000F";
+
              //Ends with X, where X is a integer denoting how many lines
              public static readonly string SectionDenoter = "///0x0000F";
+
              //Assumes inline with PropertyName immidiatetly after PropertyName
              public static readonly string ValueStart = "$$$=";            
+
              //Ends with X, where X is a integer denoting how many lines
              //Becomes the inline value of property
              //Data in collection is seperated by local EOL variable
@@ -156,11 +159,11 @@ public class ParkingRow : IParkingRow<IParkingSpace>
         /// </summary>
         /// <typeparam name="T">Output Type</typeparam>
         /// <typeparam name="U">Incoming type</typeparam>
-        public abstract class Config<T> : IConfig<T> where T : new()
+        public abstract class Config<T> : IConfig<T> 
         {
-            public Config(int cacheSize)
+            public Config(int cacheSize, T value)
             {
-                Value = new();
+                Value = value;
             }
             public string[] UnParsed { get; set; }
             static List<string> Cache = new List<string>();
@@ -191,39 +194,71 @@ public class ParkingRow : IParkingRow<IParkingSpace>
             }
             private T? CacheConverter()
             {
-                object[] result;
                 int start = Array.FindIndex(Cache.ToArray(),
                     x => x.StartsWith(IConfig<T>.SectionDenoter)
                     && x.Equals(IConfig<T>.SectionDenoter + typeof(T).Name));
 
                 int End = Array.FindIndex(Cache.ToArray(), start + 1,
                     x => x.StartsWith(IConfig<T>.SectionDenoter));
+                int? collection = 0;
+                int? collectionS;
+                Func<string, T, T>? collectionParser;
                 for (int i = start; i < End; i++)
                 {
-                                        
+                    var res = FindParser(Cache[i], out collection);
+                    if(collection >0)
+                    {
+                        collectionS = i;
+                        collectionParser = res;
+                        continue;
+                    }
+                    return res(RemoveDenoters(Cache[i], false), Value);
                 }
-                return CacheConverter();
+                return Value;
             }
-            private Func<string, T, T> FindParser(string line)
+            //this should all just be a single RegeX, alas...
+            private Func<string, T, T> FindParser(string line, out int? collection)
             {
                 Func<string, T, T>? parser;
                 string paramName = "";
                 for (int k = 0; k < Parsers.Count(); k++)
-                    {
-                        string registeredParsers = Parsers.Keys.ToList()[k];
-                        if (Cache[i].Contains(registeredParsers))
-                        {
-                            string parsername = (paramName = Cache[i].Remove(0,
-                            IConfig<T>.PropertyDenoter.Length));
-                            parser = Parsers[paramName];
-                        }
-                        if (parser is not null)
-                        {
-                            var value = Cache[i].Remove()
-                            Value = parser(paramName, Value);
-                        }
-                        else UnParsed[k] = Cache[i];
-                    }
+                {
+                    string registeredParsers = Parsers.Keys.ToList()[k];
+                    var res = ParseDenoters(line,
+                        registeredParsers,
+                        out collection);
+                    return res;
+                }
+                collection = 0;
+                return null;
+            }
+            public Func<string, T, T>? ParseDenoters(string Denoter,
+                string registeredParsers,
+                out int? collectionLen)
+            {
+                if (Denoter.Contains(registeredParsers)
+                && !Denoter.Contains(IConfig<T>.CollectionDenote))
+                {
+                    string parsername = Denoter.Remove(0,
+                    IConfig<T>.PropertyDenoter.Length);
+                    collectionLen = 0;
+                    return  Parsers[parsername];
+                }
+                else if(Denoter.Contains(
+                        registeredParsers
+                    + IConfig<T>.ValueStart
+                    + IConfig<T>.CollectionDenote))
+                {
+                    string result = Denoter.Remove(0,
+                        registeredParsers.Length
+                        + IConfig<T>.ValueStart.Length
+                        + IConfig<T>.CollectionDenote.Length
+                        + IConfig<T>.PropertyDenoter.Length);
+                    collectionLen = GetCollectionLen(result);
+                    return Parsers[registeredParsers];
+                }
+                collectionLen = 0;
+                return null;
             }
             private int GetCollectionLen(string collection)
             {
@@ -245,6 +280,13 @@ public class ParkingRow : IParkingRow<IParkingSpace>
                         i++;
                     }
                 }
+            }
+            private string RemoveDenoters(string val, bool collection)
+            {
+                if (collection)
+                    return val;
+                var res = val.IndexOf(IConfig<T>.ValueStart);
+                return val.Remove(0, res+1);
             }
         }
     }
